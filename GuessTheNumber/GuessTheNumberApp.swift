443ad9b9222a3c5f6f690c951e97a18dfef63671
Giorgio Giannotta <jorge_upa@msn.com>
2021-05-07 19:54:13 +0200

    Initial Commit

---
+++ GuessTheNumber/GuessTheNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheNumberApp.swift
+//  GuessTheNumber
+//
+//  Created by Jorge Giannotta on 07/05/21.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
